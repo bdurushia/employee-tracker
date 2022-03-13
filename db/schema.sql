fad9dd723e678b217cbcbed44f6239a870c9b0a4
bdurushia <b.durushia@gmail.com>
2022-03-13 04:21:54 -0500

    Add employee role and manager bug found

--- db/schema.sql
+++ db/schema.sql
@@ -14,8 +14,7 @@ CREATE TABLE role (
     title VARCHAR(30) NULL,
     salary DECIMAL,
     department_id INT,
-    CONSTRAINT FK_department_id FOREIGN KEY (department_id) 
-    REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -24,8 +23,38 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    CONSTRAINT FK_role_id FOREIGN KEY (role_id) 
-    REFERENCES role(id),
-    CONSTRAINT FK_manager_id FOREIGN KEY (manager_id) 
-    REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+INSERT INTO department (name)
+VALUES ('Upper Management');
+INSERT INTO department (name)
+VALUES ('Project Management');
+INSERT INTO department (name)
+VALUES ('Sales');
+INSERT INTO department (name)
+VALUES ('Finance');
+INSERT INTO department (name)
+VALUES ('Service');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('CEO', 200000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('CFO', 200000, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Head of Operations', 160000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Head of Sales', 200000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Matt', 'Majia', 1, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Doug', 'McKibbon', 3, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Janet', 'Svinovski', 2, null);
